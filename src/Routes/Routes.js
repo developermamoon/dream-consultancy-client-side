0a613ce585cbfaa3b0c74b2ecca89b410b887248
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-11-10 23:42:01 +0600

    first commit

---
+++ src/Routes/Routes.js
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Pages/Layout/Main";
+import Login from "../Pages/Login/Login";
+
+const route = createBrowserRouter([
+    {path: '/', element: <Main></Main>, children:[
+        {path: '/login', element: <Login></Login>}
+    ]}
+])
+
+export default route;
