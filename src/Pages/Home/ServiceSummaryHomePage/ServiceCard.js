44c0b41cec63595447a50c79dcaa202afcf480c4
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-11-11 18:23:29 +0600

    service page updated

--- src/Pages/Home/ServiceSummaryHomePage/ServiceCard.js
+++ src/Pages/Home/ServiceSummaryHomePage/ServiceCard.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaStar } from 'react-icons/fa';
 
 const ServiceCard = ({service}) => {
 
-    const { _id,service_name, thumbnail_url, details } = service;
+    const { _id, price, service_name, rating, thumbnail_url, details } = service;
 
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img style={{height: "240px", width: '100%'}} src={thumbnail_url} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{service_name}</h2>
+                <h2 className="card-title text-2xl">{service_name}</h2>
                 <p>{
                     details.length > 100 ? `${details.substring(0, 100)}...` : details}</p>
+
+                <p className='text-xl'> <b>Price:</b> {price ? price : "N/A"} taka</p>
+
+                <p className='flex items-center text-lg'> <b>Ratings: &nbsp;</b> {rating.number ? rating.number : "N/A"} <FaStar className='display: inline text-warning' /></p>
+
                 <div className="card-actions justify-end">
                     <Link to= {`/services/${_id}`}><button className="btn btn-warning">View Details</button></Link>
                 </div>
